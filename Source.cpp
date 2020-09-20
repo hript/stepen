1f5ebdf4241d06ba02362f2d96cb671a803e3a8d
Artem Arhipov <artemiy3893@gmail.com>
2020-09-20 17:25:15 +0300

    not working program

--- Source.cpp
+++ Source.cpp
@@ -3,15 +3,36 @@
 
 using namespace std;
 
+int stepen(int num, int step) {
+	int ans = num;
+	for (int i = 1; i <= step; i++) {
+		ans *= num;
+	}
+
+	return ans / 10;
+}
+
 int main() {
 	string num;
 	string step;
+	string lastUnitsStep;
 	cout << "Write number" << endl;
 	cin >> num;
 
 	cout << "Write degree of number" << endl;
+	cin >> step;
 
 
+	lastUnitsStep = step[step.size() - 2] + step[step.size() - 1];
+
+	char lastUnitNum = num[step.size() - 1];
+
+	if ((lastUnitsStep - '0') % (lastUnitNum - '0') == 0) {
+		cout << stepen(lastUnitNum - '0', 4);
+	}
+	else {
+		cout << stepen(lastUnitNum - '0', lastUnitsStep - '0');
+	}
 
 	return 0;
 }
