796b6e6f35bc954f157f135dfe7f0f3fbcfa3bba
Artem Arhipov <artemiy3893@gmail.com>
2020-09-20 12:49:57 +0300

    Start

---
+++ Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+#include <string>
+
+using namespace std;
+
+int main() {
+	string num;
+	string step;
+	cout << "Write number" << endl;
+	cin >> num;
+
+	cout << "Write degree of number" << endl;
+
+
+
+	return 0;
+}
